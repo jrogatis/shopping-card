e5772dd7ce706f08c22b2d90d92b03284377e35e
Jean Philip de Rogatis <jrogatis@conecta.net.br>
2017-08-13 00:48:47 -0300

    adicionei material

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import ProductsContainer from './ProductsContainer'
 import CartContainer from './CartContainer'
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-const App = () => (
+const style = {
+  marginRight: 20,
+};
+
+const Main = () => (
   <div>
     <h2>Venda sua tranqueira aqui!</h2>
+    <FloatingActionButton style={style}>
+      <ContentAdd />
+    </FloatingActionButton>
     <hr/>
     <ProductsContainer />
     <hr/>
     <CartContainer />
   </div>
 )
+
+const App = () => (
+
+  <MuiThemeProvider>
+    <Main/>
+    </MuiThemeProvider>
+)
+
 
 export default App
